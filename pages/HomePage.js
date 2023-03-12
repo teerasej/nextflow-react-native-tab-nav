bf4f077f5aeede9ed48db5bb7a94fca7dc514a35
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-03-12 22:36:10 +0700

    proviide page template

---
+++ pages/HomePage.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Box, HStack } from 'native-base'
+
+const HomePage = () => {
+  return (
+    <>
+      <Text>HomePage</Text>
+    </>
+  )
+}
+
+export default HomePage
