a1e7752e8b51173bb13cc81b83e874298310efb1
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-03-12 22:36:48 +0700

    Provide tab navigator

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NativeBaseProvider, Icon } from "native-base";
+import { FontAwesome } from '@expo/vector-icons';
+import HomePage from './pages/HomePage'
+import ChatPage from './pages/ChatPage'
+import SettingPage from './pages/SettingPage'
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home" 
+            component={HomePage} 
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon as={FontAwesome} name="home" color={color} size={size} />
+              )
+            }} 
+          />
+          <Tab.Screen
+            name="Chat" 
+            component={ChatPage} 
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon as={FontAwesome} name="comment" color={color} size={size} />
+              )
+            }} 
+          />
+          <Tab.Screen 
+            name="Setting" 
+            component={SettingPage} 
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Icon as={FontAwesome} name="gear" color={color} size={size} />
+              )
+            }} 
+          />
+        </Tab.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
